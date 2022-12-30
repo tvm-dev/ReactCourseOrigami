2dec9e9ac0610ec4b0545a4616d751997ff268a4
tvm <senadorx@gmail.com>
2022-12-30 19:14:31 -0300

    JS Review

---
+++ reviewJS/randonNumber.js
@@ -0,0 +1,3 @@
+export default function randonNumber(){
+    return Math.random()*100
+}
---
+++ reviewJS/square.js
@@ -0,0 +1,13 @@
+function squareArea(side){
+    return side * side;
+}
+
+function perimeter(side){
+    return 4 * side;
+}
+
+const square = {
+    squareArea, 
+    perimeter
+}
+export default square;
